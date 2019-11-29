c58a1cf8a0f8bf0f3b54ee5e2d0410b8b1d02fed
redstonee <1516235445@qq.com>
2019-11-29 21:29:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from bottle import *
-def makeup(x,y):
+def makeup(x, y):
     line = '{}<br/>'.format('🍋'*x)
-    shape = '<h1>Good game!</h1>'+line
+    shape = '<h1>Good game!</h1>' + line
     if y == 1:
         return shape
 
     else:
-        for shit in range(1,y):
+        for shit in range(1, y):
             shape += line
 
         return shape
 
 @route('/:x/:y')
-def index(x,y):
-    return makeup(int(x),int(y))
+def index(x, y):
+    return makeup(int(x), int(y))
 
 @route('/')
 def hello():
     return "<b>What's your problem?</b>"
 
-run(host = '0.0.0.0',port=80)
+run(host = '0.0.0.0', port = 80)
